f188b656e07f35a308c14f115eff39a11dd98903
farid95g <faridg.1995@gmail.com>
2024-06-23 19:13:20 +0400

    feat: create reusable api object

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Layout } from '@smyt/components'
+import { api } from '@smyt/api'
+import { Post } from '@smyt/types'
 
 export const App: React.FC = () => {
+  const [posts, setPosts] = useState<Post[]>([])
+
+  useEffect(() => {
+    const requestParams: RequestParams = {
+      params: { _start: 0, _limit: 10 }
+    }
+
+    const fetchData = async () => {
+      const response = await api.get('/photos', requestParams)
+      setPosts(response.data as Post[])
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <div>
       <Layout>
-        <h1>App Component</h1>
+        <div>
+          {posts.map((post: Post) => (
+            <h1 key={post.id}>{post.title}</h1>
+          ))}
+        </div>
       </Layout>
     </div>
   )
