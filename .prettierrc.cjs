a72dc2be275ca622699f903028e13bfa9281ecd9
farid95g <faridg.1995@gmail.com>
2024-06-23 14:54:07 +0400

    feat: install and configure prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  trailingComma: 'none',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  useTabs: false,
+  tabWidth: 2,
+  semi: false,
+  singleAttributePerLine: true,
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  quoteProps: 'as-needed'
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
 export const App: React.FC = () => {
-  return (
-    <div>App Component</div>
-  )
+  return <div>App Component</div>
 }
