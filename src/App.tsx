3b5ccaf12ffa0d0fcb45de3900943d1b3304f508
farid95g <faridg.1995@gmail.com>
2024-06-23 22:19:16 +0400

    feat: create Loader component; add loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react'
-import { Layout, PostList } from '@smyt/components'
+import { Layout, Loader, PostList } from '@smyt/components'
 import type { Post } from '@smyt/types'
 import { postService } from '@smyt/services'
 
 export const App: React.FC = () => {
   const [posts, setPosts] = useState<Post[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(true)
 
   useEffect(() => {
     const loadPosts = async () => {
       const posts = (await postService.loadPosts(0)) as Post[]
       setPosts(posts)
+      setIsLoading(false)
     }
 
     loadPosts()
@@ -17,9 +19,7 @@ export const App: React.FC = () => {
 
   return (
     <div>
-      <Layout>
-        <PostList posts={posts} />
-      </Layout>
+      <Layout>{isLoading ? <Loader /> : <PostList posts={posts} />}</Layout>
     </div>
   )
 }
